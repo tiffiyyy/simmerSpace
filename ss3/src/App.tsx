66fbf84110059e7281058fdfd77bb69201be01d4
Linus <linuswong9@gmail.com>
2025-09-05 18:12:28 -0700

    real ingredients list

--- ss3/src/App.tsx
+++ ss3/src/App.tsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react'
+import './App.css'
 
-// 1. Define the type
 type Ingredient = {
   name: string;
   checked: boolean;
 };
-
-export default function IngredientChecklist() {
-  // 2. Initial ingredient list
+function App() {
   const [ingredients, setIngredients] = useState<Ingredient[]>([
     { name: "Tomato", checked: false },
     { name: "Cheese", checked: false },
     { name: "Basil", checked: false },
   ]);
 
-  // 3. Function to toggle a checkbox
   const toggleIngredient = (index: number) => {
     const newIngredients = [...ingredients]; // copy array
     newIngredients[index].checked = !newIngredients[index].checked; // flip the value
@@ -22,22 +19,26 @@ export default function IngredientChecklist() {
   };
 
   return (
-    <div className="flex">
-      <h2>Ingredient Checklist</h2>
-      <ul className="list-none p-0 m-0">
-        {ingredients.map((ingredient, index) => (
-          <li key={ingredient.name}>
-            <label>
-              <input
-                type="checkbox"
-                checked={ingredient.checked}
-                onChange={() => toggleIngredient(index)}
-              />
-              {ingredient.name}
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+      <div>
+        <h2>Ingredient Checklist</h2>
+        <ul style={{ listStyleType: "none", padding: 0, margin: 0,}}>
+          {ingredients.map((ingredient, index) => (
+            <li key={ingredient.name}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={ingredient.checked}
+                  onChange={() => toggleIngredient(index)}
+                />
+                {ingredient.name}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
+
+export default App

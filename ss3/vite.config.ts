bce9147b486072a64a9e83740a3128458b2d0620
tiffiyyy <Tiffany.Le710@outlook.com>
2025-09-09 18:44:59 -0700

    restored original vite.config.ts file

--- ss3/vite.config.ts
+++ ss3/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import webSpatial from "@webspatial/vite-plugin";
 import { createHtmlPlugin } from "vite-plugin-html";
 
@@ -12,12 +12,8 @@ export default defineConfig({
       inject: {
         data: {
           XR_ENV: process.env.XR_ENV,
-          XR_ENV_BASE: process.env.XR_ENV === 'visionos' ? '/spatial' : '',
         },
       },
-    }),  
+    }),
   ],
-  define: {
-    __XR_ENV_BASE__: JSON.stringify(process.env.XR_ENV === 'visionos' ? '/spatial' : ''),
-  },
-})
+});
